33712ecb177e16e201ef832eca915308c028b74c
Sero <69639595+Seroxdesign@users.noreply.github.com>
2024-05-06 07:11:52 -0400

    Update and rename MyToken_test.sol to Fungees_test.sol

--- tests/Fungees_test.sol
+++ tests/Fungees_test.sol
@@ -3,7 +3,7 @@
 pragma solidity >=0.7.0 <0.9.0;
 import "remix_tests.sol";
 import "remix_accounts.sol";
-import "../contracts/MyToken.sol";
+import "../contracts/Fungees.sol";
 
 contract MyTokenTest {
 
@@ -12,11 +12,11 @@ contract MyTokenTest {
     function beforeAll () public {
         address acc0 = TestsAccounts.getAccount(0);
         // acc0 will be set as initial owner
-        s = new MyToken(acc0);
+        s = new Fungees(acc0);
     }
 
     function testTokenNameAndSymbol () public {
-        Assert.equal(s.name(), "MyToken", "token name did not match");
-        Assert.equal(s.symbol(), "MTK", "token symbol did not match");
+        Assert.equal(s.name(), "Fungees", "token name did not match");
+        Assert.equal(s.symbol(), "FUN", "token symbol did not match");
     }
 }
